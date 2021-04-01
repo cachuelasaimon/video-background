f01e5c2588a3f30d84ea367168afd4b49f715314
cachuelasaimon <cachuelasaimon@gmail.com>
2021-04-01 12:33:27 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var toggleButton = document.querySelector('.toggle')
+    var showcase = document.querySelector(".showcase")
+    toggleButton.classList.toggle("active")
+    showcase.classList.toggle('active')
+}
